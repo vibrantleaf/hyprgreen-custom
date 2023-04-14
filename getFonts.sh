7506dcf69959404f805aaa7f3ca4d44e0ac9a734
julia <julia@volcanocoffee.monster>
2023-04-15 03:41:41 +0800

    update getFonts.sh

--- getFonts.sh
+++ getFonts.sh
@@ -1,3 +1,5 @@
+mkdir -p /usr/local/share/fonts/
+
 dnf install -y \
  wget \
  fontconfig \
@@ -15,6 +17,6 @@ dnf install -y \
 
 rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
-mkdir /usr/local/share/fonts/ComicMonoNF
+mkdir -p /usr/local/share/fonts/ComicMonoNF
 wget -O /usr/local/share/fonts/ComicMonoNF/ComicMonoBold-NF.ttf https://github.com/vibrantleaf/comic-mono-font-NF/releases/download/2022-12-3/Comic.Mono.Bold.Nerd.Font.Complete.ttf
 wget -O /usr/local/share/fonts/ComicMonoNF/ComicMono-NF.ttf https://github.com/vibrantleaf/comic-mono-font-NF/releases/download/2022-12-3/Comic.Mono.Nerd.Font.Complete.ttf
